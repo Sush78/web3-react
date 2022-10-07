3d29d920a8f3e80e0bb2436f2c6d4f52b48e18c9
Sushant Kumar <35889186+Sush78@users.noreply.github.com>
2022-10-07 11:29:11 +0100

    removed

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json'
 
 export const contractABI = abi.abi
-export const contractAddress = '0x97444640e3C406C729FD680356eDe1D1d4Aa733c'
+export const contractAddress = 'Enter Address HERE'
