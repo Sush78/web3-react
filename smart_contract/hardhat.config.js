aab4ff4fb3ca704e12adda72a90aa77ad9f859f9
Sushant Kumar <35889186+Sush78@users.noreply.github.com>
2022-10-12 15:09:12 +0100

    Abstract credentials

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,8 +6,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/ZanUtb13BHboslQ2wHDlYdjF7sPRHqj6",
-      accounts: ['1ea0429d0e67a6dd4f34e5b81d7068042fab9dc8efd123531d94f3835269143a']
+      url: "insert url here",
+      accounts: ['insert account here']
     }
   }
 }
